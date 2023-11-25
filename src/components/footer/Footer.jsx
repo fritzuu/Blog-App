d3008b9d54eef012c13c6d4ab24383226930212a
Revan Alifian Zhafran <igtrvnaz@gmail.com>
2023-11-26 00:00:38 +0700

    adding layouts and components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            2023 RJRVerse. All rights reserved.
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
