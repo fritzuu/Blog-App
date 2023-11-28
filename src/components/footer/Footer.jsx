91616936527e57811664b7c839d4532cac265d02
Revan Alifian Zhafran <igtrvnaz@gmail.com>
2023-11-28 14:55:08 +0700

    Adding Footer Ui

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
-        <div>
-            2023 RJRVerse. All rights reserved.
-        </div>
-        <div></div>
+    <div className={styles.container}>
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={30} height={30} className={styles.icon} alt="Facebook" />
+        <Image src="/2.png" width={30} height={30} className={styles.icon} alt="Instagram" />
+        <Image src="/3.png" width={30} height={30} className={styles.icon} alt="Twitter" />
+        <Image src="/4.png" width={30} height={30} className={styles.icon} alt="Youtube" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
