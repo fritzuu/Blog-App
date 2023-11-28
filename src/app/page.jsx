11d23e64353f7f95cfcaf1608b63fcefecf78d25
Revan Alifian Zhafran <igtrvnaz@gmail.com>
2023-11-28 16:05:33 +0700

    Add Home Ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,20 @@
 import Image from 'next/image'
+import Hero from 'public/hero.png'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+      <h1 className={styles.title}>Your Favourite Information Website</h1>
+      <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, repudiandae! Reprehenderit placeat tenetur commodi veritatis ut inventore, adipisci sequi obcaecati non!</p>
+      <Button url="/portfolio
+          " text="See More?" />
+      </div>
+      <div className={styles.item}>
+      <Image src={Hero} alt="" className={styles.img}/>
+      </div>
+    </div>
   )
 }
